0d20ba6937394a53d3190f6229ad0c9faa5d1ad9
ZachRamos <zachariramos@gmail.com>
2019-08-05 18:04:44 -0500

    deleted and added day1.py back

---
+++ day1.py
@@ -0,0 +1,19 @@
+import sys
+# Declare second integer, double, and String variables.
+i1 = 0
+d1 = 0.0
+s1 = ""
+
+# Read and save an integer, double, and String to your variables.
+i1 = int(sys.stdin.readline())
+d1 = float(sys.stdin.readline())
+s1 = sys.stdin.readline()
+
+# Print the sum of both integer variables on a new line.
+print(i + i1)
+
+# Print the sum of the double variables on a new line.
+print(d + d1)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s1)
